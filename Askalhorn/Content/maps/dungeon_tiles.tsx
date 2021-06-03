a9d7835b947357c601b382a1138da9398c05200e
Gandifil <Gandifilmk@gmail.com>
2021-06-03 09:40:54 +0300

    Add resources.

---
+++ Askalhorn/Content/maps/dungeon_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="dungeon_tiles" tilewidth="64" tileheight="64" tilecount="192" columns="16">
+ <image source="64x64.png" width="1024" height="768"/>
+</tileset>
