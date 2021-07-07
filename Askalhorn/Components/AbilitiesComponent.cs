33976c513c63f846d191ee50310d714a366876f6
Gandifil <Gandifilmk@gmail.com>
2021-07-07 23:45:25 +0300

    Add gui magic ability block.

--- Askalhorn/Components/AbilitiesComponent.cs
+++ Askalhorn/Components/AbilitiesComponent.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework;
 using MLEM.Extended.Extensions;
 using MLEM.Ui;
 using MLEM.Ui.Elements;
+using MLEM.Ui.Style;
 
 namespace Askalhorn.Components
 {
@@ -19,9 +20,11 @@ namespace Askalhorn.Components
         private class AbilityBox
         {
             public readonly Panel Panel;
+            public Image Image;
             public IAbility _ability;
+            private readonly ICharacter character;
 
-            public AbilityBox(Panel parent, int index)
+            public AbilityBox(Panel parent, int index, ICharacter character)
             {
                 Panel = new Panel(Anchor.AutoInlineIgnoreOverflow, new Vector2(0.1f, 1), Vector2.Zero);
                 Panel.AddChild(new Paragraph(Anchor.BottomRight, 20, index.ToString())
@@ -29,23 +32,36 @@ namespace Askalhorn.Components
                     TextScaleMultiplier = 0.7f,
                 });
                 parent.AddChild(Panel);
+
+                this.character = character;
+                character.HP.Current.Changed += () => UpdateMagic();
+            }
+
+            private void UpdateMagic()
+            {
+                if (_ability is not null)
+                    Image.Color = new StyleProp<Color>(character.MP.Current.Value < _ability.MagicCost ? Color.Red : Color.Transparent);
             }
 
             public void SetEffect(IAbility ability)
             {
                 _ability = ability;
-                var image = new Image(Anchor.Center, new Vector2(1, 1), ability.Icon.ToMlem());
-                image.CanBeMoused = true;
-                image.OnPressed += _ => Run();
-                var tooltip = new Tooltip(500, ability.ToString(), image);
+                Image = new Image(Anchor.Center, new Vector2(1, 1), ability.Icon.ToMlem());
+                Image.CanBeMoused = true;
+                Image.OnPressed += _ => Run();
+                var tooltip = new Tooltip(500, ability.ToString(), Image);
                 tooltip.MouseOffset = new Vector2(32, -64);
-                Panel.AddChild(image);
+                Panel.AddChild(Image);
+                UpdateMagic();
             }
 
             public void Run()
             {
-                if (_ability is not null) 
+                if (_ability is not null)
+                {
                     World.Instance.playerController.AddMove(new UseAbilityMove(_ability));
+                    
+                }
             }
         }
 
@@ -66,8 +82,8 @@ namespace Askalhorn.Components
             var box = new Panel(Anchor.BottomRight, new Vector2(0.7f, 0.1f), Vector2.Zero);
 
             for (int i = 1; i < ABILITIES_COUNT; i++)
-                boxes[i] = new AbilityBox(box, i);
-            boxes[0] = new AbilityBox(box, 0);
+                boxes[i] = new AbilityBox(box, i, character);
+            boxes[0] = new AbilityBox(box, 0, character);
             
             for (int i = 0; i < 3; i++)
                 boxes[i+1].SetEffect(character.Abilities.ElementAt(i));
