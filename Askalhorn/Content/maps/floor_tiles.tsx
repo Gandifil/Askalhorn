d90452180f0ebe7f5458cb3398afb4b15d232413
Gandifil <Gandifilmk@gmail.com>
2021-09-05 00:44:31 +0300

    Add more size for start map.

---
+++ Askalhorn/Content/maps/floor_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floor_tiles" tilewidth="64" tileheight="32" tilecount="1" columns="1">
+ <image source="floor_tiles.png" width="64" height="32"/>
+</tileset>
