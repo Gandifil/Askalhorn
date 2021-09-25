f2ccbfc3180e216793e6a628cdc7da0b96b68c32
Gandifil <Gandifilmk@gmail.com>
2021-09-25 18:03:36 +0300

    Add new item impls for reading item data from file.

---
+++ Askalhorn.Inventory/BagFillers/LootChooser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Askalhorn.Common.Interpetators;
+using Askalhorn.Inventory.Items;
+using Newtonsoft.Json;
+
+namespace Askalhorn.Inventory.BagFillers
+{
+    public class LootChooser: ILootChooser
+    {
+        public IItem Item { get; }
+        
+        public IExpression<uint> Count { get; }
+
+        [JsonConstructor]
+        public LootChooser(IItem item, IExpression<uint> count)
+        {
+            Item = item;
+            Count = count;
+        }
+        
+        public void Fill(Random random, IBag bag)
+        {
+            var count = Count.Generate(new ExpressionArgs
+            {
+                Random = random,
+            });
+            
+            bag.Put(Item, count);
+        }
+    }
+}
---
+++ Askalhorn.Inventory/Items/ContentItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Askalhorn.Common;
+using Askalhorn.Render;
+using MonoGame.Extended.TextureAtlases;
+using Newtonsoft.Json;
+
+namespace Askalhorn.Inventory.Items
+{
+    public class ContentItem: IItem
+    {
+        public string ContentName { get; }
+
+        private readonly IItem _item;
+
+        [JsonConstructor]
+        public ContentItem(string contentName)
+        {
+            ContentName = contentName;
+            _item = Storage.Content.Load<IItem>($"items/{contentName}");
+        }
+        
+        [JsonIgnore]
+        public TextureRenderer Renderer => _item.Renderer;
+
+        [JsonIgnore]
+        public string TooltipText => _item.TooltipText;
+
+        public event Action OnChanged
+        {
+            add => _item.OnChanged += value;
+            remove => _item.OnChanged -= value;
+        }
+
+        [JsonIgnore]
+        public string Name => _item.Name;
+
+        [JsonIgnore]
+        public ItemPurpose Type => _item.Type;
+
+        [JsonIgnore]
+        public ItemRarity ItemRarity => _item.ItemRarity;
+
+        [JsonIgnore]
+        public float Weight => _item.Weight;
+
+        [JsonIgnore]
+        public IImpact Impact => _item.Impact;
+
+        public bool Equals(IItem? other)
+        {
+            return _item.Equals(other);
+        }
+    }
+}
