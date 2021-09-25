e29ccf4d7d5004cdbdfb3371d3243449a62fdee4
Gandifil <Gandifilmk@gmail.com>
2021-09-25 18:03:49 +0300

    Add gold coin.

---
+++ Askalhorn.Inventory/Items/Coin.cs
@@ -0,0 +1,13 @@
+﻿using Askalhorn.Common;
+using MonoGame.Extended.TextureAtlases;
+
+namespace Askalhorn.Inventory.Items
+{
+    public class Coin: Item
+    {
+        public override ItemPurpose Type => ItemPurpose.Currency;
+        public override ItemRarity ItemRarity => ItemRarity.Casual;
+        public override float Weight => .008f;
+        protected override IImpact Impact => null;
+    }
+}
